f03ed541014c2baaec78a77447edbe1e76c6fd1c
Mitesh Shah <mitesh@miteshshah.com>
2020-04-18 02:42:29 +0530

    wip: blog editor

--- front/src/components/AddPost.jsx
+++ front/src/components/AddPost.jsx
@@ -1,11 +1,210 @@
-import React from "react";
+import React, { useState, useCallback, useMemo } from "react";
+import { Slate, Editable, withReact } from "slate-react";
+import { Editor, Transforms, Range, Point, createEditor } from "slate";
+import { withHistory } from "slate-history";
 
-function AddPost() {
-  return (
-    <div>
-      <p>Add Post</p>
-    </div>
+const SHORTCUTS = {
+  "*": "list-item",
+  "-": "list-item",
+  "+": "list-item",
+  ">": "block-quote",
+  "#": "heading-one",
+  "##": "heading-two",
+  "###": "heading-three",
+  "####": "heading-four",
+  "#####": "heading-five",
+  "######": "heading-six",
+  "```": "code-block",
+};
+
+const AddPost = () => {
+  const [value, setValue] = useState(initialValue);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const editor = useMemo(
+    () => withLayout(withShortcuts(withReact(withHistory(createEditor())))),
+    []
   );
-}
+  return (
+    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
+      <Editable
+        renderElement={renderElement}
+        placeholder="Write some markdown..."
+        spellCheck
+        autoFocus
+      />
+    </Slate>
+  );
+};
+
+const withLayout = (editor) => {
+  return editor;
+  const { normalizeNode } = editor;
+
+  editor.normalizeNode = ([node, path]) => {
+    if (path.length === 0) {
+      if (editor.children.length < 1) {
+        const title = { type: "title", children: [{ text: "Untitled" }] };
+        Transforms.insertNodes(editor, title, { at: path.concat(0) });
+      }
+
+      if (editor.children.length < 2) {
+        const paragraph = { type: "paragraph", children: [{ text: "" }] };
+        Transforms.insertNodes(editor, paragraph, { at: path.concat(1) });
+      }
+
+      for (const [child, childPath] of Node.children(editor, path)) {
+        const type = childPath[0] === 0 ? "title" : "paragraph";
+
+        if (child.type !== type) {
+          Transforms.setNodes(editor, { type }, { at: childPath });
+        }
+      }
+    }
+
+    return normalizeNode([node, path]);
+  };
+
+  return editor;
+};
+
+const withShortcuts = (editor) => {
+  const { deleteBackward, insertText } = editor;
+
+  editor.insertText = (text) => {
+    const { selection } = editor;
+    console.log(selection);
+
+    if (text === " " && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = Editor.above(editor, {
+        match: (n) => Editor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      console.log(path);
+      const start = Editor.start(editor, path);
+      const range = { anchor, focus: start };
+      console.log(range);
+      const beforeText = Editor.string(editor, range);
+      console.log(beforeText);
+      const type = SHORTCUTS[beforeText];
+      console.log("-----");
+
+      if (type) {
+        Transforms.select(editor, range);
+        Transforms.delete(editor);
+        Transforms.setNodes(
+          editor,
+          { type },
+          { match: (n) => Editor.isBlock(editor, n) }
+        );
+
+        if (type === "list-item") {
+          console.log("list");
+          const list = { type: "bulleted-list", children: [] };
+          Transforms.wrapNodes(editor, list, {
+            match: (n) => n.type === "list-item",
+          });
+        }
+
+        return;
+      }
+    }
+
+    insertText(text);
+  };
+
+  editor.deleteBackward = (...args) => {
+    const { selection } = editor;
+
+    if (selection && Range.isCollapsed(selection)) {
+      const match = Editor.above(editor, {
+        match: (n) => Editor.isBlock(editor, n),
+      });
+
+      if (match) {
+        const [block, path] = match;
+        const start = Editor.start(editor, path);
+
+        if (
+          block.type !== "paragraph" &&
+          Point.equals(selection.anchor, start)
+        ) {
+          Transforms.setNodes(editor, { type: "paragraph" });
+
+          if (block.type === "list-item") {
+            Transforms.unwrapNodes(editor, {
+              match: (n) => n.type === "bulleted-list",
+              split: true,
+            });
+          }
+
+          return;
+        }
+      }
+
+      deleteBackward(...args);
+    }
+  };
+
+  return editor;
+};
+
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case "title":
+      return <h2 {...attributes}>{children}</h2>;
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "heading-three":
+      return <h3 {...attributes}>{children}</h3>;
+    case "heading-four":
+      return <h4 {...attributes}>{children}</h4>;
+    case "heading-five":
+      return <h5 {...attributes}>{children}</h5>;
+    case "heading-six":
+      return <h6 {...attributes}>{children}</h6>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "code-block":
+      return (
+        <CodeElement
+          attributes={attributes}
+          children={children}
+          element={element}
+        />
+      );
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const initialValue = [
+  {
+    type: "title",
+    children: [{ text: "Title" }],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: "Start writing...",
+      },
+    ],
+  },
+];
 
 export default AddPost;
